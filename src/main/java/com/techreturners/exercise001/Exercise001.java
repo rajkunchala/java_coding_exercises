206377d107ded61ac07264edd47cd2f207afe314
raja <kunchala@yahoo.com>
2021-09-02 14:09:48 +0100

    Exercise001 solved

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,39 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        return word.substring(0,1).toUpperCase()+word.substring(1,word.length()) ;
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        return firstName.charAt(0)+"."+lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        double totalPrice = originalPrice + ((originalPrice/100)*vatRate);
+        BigDecimal bd = new BigDecimal(totalPrice).setScale(2, RoundingMode.DOWN);
+
+        return bd.doubleValue();
     }
 
     public String reverse(String sentence) {
         // Add your code here
-        return "";
+        return new StringBuilder(sentence).reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
-        return 0;
+        long linuxUsers = users.stream()
+                                 .filter(u-> u.getType().equals("Linux"))
+                                 .count();
+
+        return (int)linuxUsers;
     }
 }
