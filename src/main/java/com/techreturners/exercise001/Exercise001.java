ffb041e0a9bdd653ff17a0285931cd60bf7a8f00
raja <kunchala@yahoo.com>
2021-09-03 11:31:32 +0100

    Remove unnecessary index for substring in method
    Removed unnecessary endindex from the substring in method capitalizeWord

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -6,17 +6,17 @@ import java.math.RoundingMode;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return word.substring(0,1).toUpperCase()+word.substring(1,word.length()) ;
+
+        return word.substring(0,1).toUpperCase()+word.substring(1) ;
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
+
         return firstName.charAt(0)+"."+lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
+
         double totalPrice = originalPrice + ((originalPrice/100)*vatRate);
         BigDecimal bd = new BigDecimal(totalPrice).setScale(2, RoundingMode.DOWN);
 
@@ -24,12 +24,12 @@ public class Exercise001 {
     }
 
     public String reverse(String sentence) {
-        // Add your code here
+
         return new StringBuilder(sentence).reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
+
         long linuxUsers = users.stream()
                                  .filter(u-> u.getType().equals("Linux"))
                                  .count();
