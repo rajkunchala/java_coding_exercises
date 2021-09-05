d2a1a24dc051a88281a590b7609d05dc9eb07e4e
raja <kunchala@yahoo.com>
2021-09-05 21:46:40 +0100

    Added new tests in Exercise005Test
    Added two more tests to check for pangram

--- src/test/java/com/techreturners/exercise005/Exercise005Test.java
+++ src/test/java/com/techreturners/exercise005/Exercise005Test.java
@@ -61,4 +61,15 @@ public class Exercise005Test {
         assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
 
+    @Test
+    public void checkPerfectLowerCaseSentenceWithSpacesIsAPangram() {
+        assertTrue(ex005.isPangram("a b c d e f g h i j k l m n o p q r s t u v w x y z"));
+    }
+
+    @Test
+    public void checkPerfectUpperCaseSentenceWithSpacesIsAPangram() {
+        assertTrue(ex005.isPangram("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"));
+    }
+
+
 }
