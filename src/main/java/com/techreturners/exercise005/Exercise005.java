58cc1de56284623bdb041ee206af44b5cc707cb4
raja <kunchala@yahoo.com>
2021-09-05 13:58:33 +0100

    Exercise005 completed with initial tests
    completed to pass initial tests. refactor and additonal tests pending

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,45 @@
 package com.techreturners.exercise005;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Exercise005 {
 
+    
+    
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+
+        String inputLowerCase = input.toLowerCase(Locale.ROOT);
+
+        List<String> inputCharList = new ArrayList<String>();
+
+        // reference list for alphabets
+        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+        //convert input to a list, so we can search the list
+        for (int i = 0; i < inputLowerCase.length() ; i++) {
+            
+            inputCharList.add(inputLowerCase.charAt(i)+"");
+
+        }
+
+        boolean result = true;
+        //now search the input for the alphabets
+        for (int i = 0; i <alphabets.length() ; i++) {
+
+            if (inputCharList.contains( alphabets.charAt(i)+"")){
+            }
+            else {
+                result = false;
+            }
+        
+        
+
+
+        }
+        return result;
+
     }
 
 }
