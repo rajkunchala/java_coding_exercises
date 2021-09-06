b124e9a7791d7bf13f46e3ef661ef4ef69ba8cfd
raja <kunchala@yahoo.com>
2021-09-06 15:00:36 +0100

    Cleanup Execise002
    Removed unnecessary comments and whitespace

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,14 +3,12 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester( Person person ) {
-        // Add your code here
-
 
         return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm( Person person, int ageLimit) {
-        // Add your code here
+        
         return person.getAge()>=ageLimit;
     }
     
